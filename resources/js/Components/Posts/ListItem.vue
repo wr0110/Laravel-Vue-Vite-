a1be55ec10bedcfef598efc811d70ce131390fcb
silver-ts <silvertechsavvy@outlook.com>
2022-07-18 04:19:41 +0800

    fix styles

--- resources/js/Components/Posts/ListItem.vue
+++ resources/js/Components/Posts/ListItem.vue
@@ -4,9 +4,8 @@
       <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">{{ item.category.name }}</a>
       <Link :href="`/posts/${item.slug}`" class="text-3xl font-bold hover:text-gray-700 pb-4">{{ item.title }}</Link>
       <p href="#" class="text-sm pb-3">
-        By <a href="#" class="font-semibold hover:text-gray-800">{{ item.author.name }}</a>, {{
-            item.published_at_in_time_ago
-        }} ago
+        By <a href="#" class="font-semibold hover:text-gray-800">{{ item.author.name }}</a>,
+        {{ item.published_at_in_time_ago }} ago
       </p>
       <a href="#" class="pb-6">{{ item.content }}</a>
       <a href="#" class="uppercase text-gray-800 hover:text-black hover:font-bold">
--- resources/js/Pages/Posts/Show.vue
+++ resources/js/Pages/Posts/Show.vue
@@ -3,7 +3,7 @@
   <Head :title="post.title" />
 
   <div class="flex sm:justify-center py-6">
-    <article class="w-full md:w-2/3">
+    <article class="w-full md:w-2/3 px-3">
       <h1 class="text-3xl font-bold hover:text-gray-700 pb-4">{{ post.title }}</h1>
       <p>{{ post.content }}</p>
     </article>
