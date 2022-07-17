8143d95e36d5dbdffc613893ddebc924616961cd
silver-ts <silvertechsavvy@outlook.com>
2022-07-18 03:58:54 +0800

    change post list page layout

--- resources/js/Components/Posts/FeaturedList.vue
+++ resources/js/Components/Posts/FeaturedList.vue
@@ -1,4 +1,6 @@
 <template>
+  <h1 class="font-bold text-gray-800 uppercase hover:text-gray-700 text-3xl py-5">Featured artitles</h1>
+
   <ul class="divide-y divide-gray-100 w-full">
     <li class="p-3 text-gray-500 hover:bg-gray-100 hover:text-black flex flex-col" v-for="item in items">
       <a href="#" class="text-xl font-bold hover:text-gray-700 pb-2">{{ item.title }}</a>
--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -6,13 +6,11 @@
     <section class="w-full md:w-2/3 px-3">
       <PostList :items="posts.data" />
 
-      <Pagination :pageUrl="$page.url" :current="posts.current_page" :total="posts.total" :perPage="posts.per_page"
+      <Pagination :pageUrl="$page.url" :current="posts.meta.current_page" :total="posts.meta.total" :perPage="posts.meta.per_page"
         :only="['posts']" @page-changed="changePage" />
     </section>
 
     <aside class="w-full md:w-1/3 px-3">
-      <h1 class="font-bold text-gray-800 uppercase hover:text-gray-700 text-4xl py-5">Featured artitles</h1>
-
       <FeaturedPostList :items="featured_posts" />
     </aside>
   </div>
